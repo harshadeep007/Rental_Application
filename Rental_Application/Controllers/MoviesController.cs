27c349bcb87a9cdaf06bde14a7516e0c93f84cc0
Harshadeep <harshadeep007@gmail.com>
2017-05-18 08:43:32 +0530

    Added GenreClass and some features to movies view

--- Rental_Application/Controllers/MoviesController.cs
+++ Rental_Application/Controllers/MoviesController.cs
@@ -4,25 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Rental_Application.Models;
+using System.Data.Entity;
 
 namespace Rental_Application.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _Context;
+
+
+        public MoviesController()
+        {
+            _Context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _Context.Dispose();
+
+        }
         // GET: Movies
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _Context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
+            var movie = _Context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.id == id);
+            if (movie == null)
             {
-                new Movie {id=1,MovieName="Fast & Furious 8" },
-                new Movie {id=2,MovieName="Need For Speed" }
-            };
+                return HttpNotFound();
+            }
+            return View(movie);
+
         }
+
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie {id=1,MovieName="Fast & Furious 8" },
+        //        new Movie {id=2,MovieName="Need For Speed" }
+        //    };
+        //}
     }
 }
